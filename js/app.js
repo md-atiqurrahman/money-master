6a3b3b02f5ea7f5a2de749e195ee9395c3540de2
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-02-17 22:30:32 +0600

    finish calculation

--- js/app.js
+++ js/app.js
@@ -1,29 +1,53 @@
+function showMessage(){
+    
+}
 //get the input field number
 function getInputNumber(inputId){
     const input = document.getElementById(inputId);
     const inputText = input.value;
     const inputNumber = parseFloat(inputText);
-
     //clear input field
     input.value = '';
     return inputNumber;
 }
+//get the span element
 // add handler in calculate button
 document.getElementById('calculate-btn').addEventListener('click',function(){
-    const foodInputNumber = getInputNumber('food-input');
-    const rentInputNumber = getInputNumber('rent-input');
-    const clothesInputNumber = getInputNumber('clothes-input');
+        const foodInputNumber = getInputNumber('food-input');
+        const rentInputNumber = getInputNumber('rent-input');
+        const clothesInputNumber = getInputNumber('clothes-input');
+        
+        const totalCost = foodInputNumber + rentInputNumber + clothesInputNumber;
+        
+        const totallExpenses = document.getElementById('totall-expenses');
+        totallExpenses.innerText = totalCost;
+        const totallExpensesNumber = parseFloat(totallExpenses.innerText);
     
-    const totalCost = foodInputNumber + rentInputNumber + clothesInputNumber;
+        const incomeInputNumber = getInputNumber('income-input');
     
-    const totallExpenses = document.getElementById('totall-expenses');
-    totallExpenses.innerText = totalCost;
-    const totallExpensesNumber = parseFloat(totallExpenses.innerText);
+        const balance = incomeInputNumber - totallExpensesNumber;
+    
+        const totallBalance = document.getElementById('totall-balance');
+        totallBalance.innerText = balance;
+})
 
+document.getElementById('save-btn').addEventListener('click',function(){
+    // update saving amount
+    const saveInputNumber = getInputNumber('save-input');
     const incomeInputNumber = getInputNumber('income-input');
 
-    const balance = incomeInputNumber - totallExpensesNumber;
+    const incomePercentage = incomeInputNumber / saveInputNumber;
 
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = incomePercentage;
+    const savingAmountNumber = parseFloat( savingAmount.innerText);
+     //update remaining balance 
     const totallBalance = document.getElementById('totall-balance');
-    totallBalance.innerText = balance;
+    const totallBalanceNumber= parseFloat(totallBalance.innerText);
+
+
+     const remainingBalanceNumber = totallBalanceNumber - savingAmountNumber;
+
+    const remainingBalance = document.getElementById('remaining-balance');
+    remainingBalance.innerText = remainingBalanceNumber;
 })
